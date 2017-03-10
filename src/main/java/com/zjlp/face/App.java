1ee155412ece179f0af5e748c9a4b6714122267a
zj-lingxin <zj_lingxin@163.com>
2017-03-10 09:36:46 +0800

    update

---
+++ src/main/java/com/zjlp/face/App.java
@@ -0,0 +1,8 @@
+package com.zjlp.face;
+
+/**
+ * Created by root on 3/9/17.
+ */
+public class App {
+
+}
---
+++ src/main/java/com/zjlp/face/SimpleProducer.java
@@ -0,0 +1,43 @@
+package com.zjlp.face;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import java.util.Properties;
+
+/**
+ * 代码参看：https://www.tutorialspoint.com/apache_kafka/apache_kafka_simple_producer_example.htm
+ */
+public class SimpleProducer {
+    public static void main(String args[]) {
+        if (args.length == 0) {
+            System.out.println("Enter topic name");
+            return;
+        }
+        String topicName = args[0].toString();
+        Properties props = new Properties();
+        props.put("client.id","112");
+        props.put("bootstrap.servers","192.168.175.12:9092,192.168.175.11:9092");
+        props.put("acks","all");
+        //Setting a value greater than zero will cause the producer to resend any record whose send fails with a potentially transient error
+        props.put("retries", 0);
+        //Specify buffer size in config
+        props.put("batch.size", 16384);
+        //Reduce the no of requests less than 0
+        props.put("linger.ms", 100);
+        //The buffer.memory controls the total amount of memory available to the producer for buffering.
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<String, String>(props);
+
+        for(int i = 0; i < 10; i++)
+            producer.send(new ProducerRecord<String, String>(topicName,
+                    Integer.toString(i), Integer.toString(i)+"vv"));
+        System.out.println("Message sent successfully");
+        producer.close();
+    }
+}
